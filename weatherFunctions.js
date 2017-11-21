fd7a16b4037fc2668ea4439a00650d492a360248
colin eagle <coeagle@students.lltc.edu>
2017-11-21 16:32:54 +0000

    here it is

--- weatherFunctions.js
+++ weatherFunctions.js
@@ -1,14 +1,14 @@
 function calculateDewPoint (temp, humidity)
 {
     var dewp;
-    dewp = temp-(9(100 - humidity)/25);
+    dewp = temp - 9 * (100 - humidity)/25;
     return dewp; 
 }
 
 function calculateWindChill (temp, wind)
 {
     var winchill;
-    winchill = Math.pow (wind,0.16) * 35.74 + 0.6215 * temp + (0.4275 * temp - 35.75) ;
+    winchill = 35.74 + 0.6215 * temp + (0.4275 * temp - 35.75) * Math.pow (wind,0.16) ;
     return winchill;
     
 }
